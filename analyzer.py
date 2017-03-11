0812337959f7e927c003f61a4383fdef998cc70e
Feryandi Nurdiantoro <feryandi.n@gmail.com>
2017-03-11 15:16:31 +0700

    References

--- analyzer.py
+++ analyzer.py
@@ -34,12 +34,8 @@ class Analyzer:
 			i += 1
 		return dataset
 
-	def __best_reference(self, dataset, label):
+	def __get_references(self, dataset, label):
 		meta = sorted(dataset, key=lambda x: (x.date, x.url_score), reverse=True)
-		selected = []
-		for m in meta:
-			if m.label == label:
-				selected.append(m)	
 		return selected
 
 	def do(self):
@@ -72,12 +68,13 @@ class Analyzer:
 
 		ridx = self.__do_voting(conclusion)
 
-		references = self.__best_reference(dataset, Analyzer.target[ridx])
+		references = self.__get_references(dataset, Analyzer.target[ridx])
 		lor = []
 		for r in references:
 			data = {}
 			data["url"] = r.url
-			data["content"] = r.content
+			data["url_base"] = r.url_base
+			data["label"] = r.label
 			lor.append(data)
 
 		result = {}
