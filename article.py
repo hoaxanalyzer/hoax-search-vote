99c856a18f1f03ea138107125c329fff81803030
Feryandi Nurdiantoro <feryandi.n@gmail.com>
2017-04-11 17:43:08 +0700

    Add more Clue Words

--- article.py
+++ article.py
@@ -49,7 +49,7 @@ class Article:
 		"tidak berhubungan", "tidak menemukan hubungan", "tidak ditemukan hubungan", "untrue", "no other scientists",\
 		"dishonestly", "irresponsibly", "still believe", "retracted", "falsified", "no link", "validity",\
 		"no direct correlation", "no correlation", "disreputable", "to tarnish", "no such thing", "fan-fiction", "absurdity of the claim",\
-		"absurd claim", "lacks truth",\
+		"absurd claim", "lacks truth", "urban legends", "urban legend", "no law against", "answer is no",\
 		"not been able to replicate", "implausible", "sempat dikabarkan", "error", "april fools", "april mop"])]
 
 	unkngram = [WordGram(["not clear", "tidak jelas", "cant conclude", "tidak ada kesimpulan", "tidak bisa disimpulkan",\
